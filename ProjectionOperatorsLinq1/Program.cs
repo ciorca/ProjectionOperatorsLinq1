58f67e6549942eff79140140092c27ff41a086df
ciorca <ciorca.gaby@gmail.com>
2022-06-18 14:39:19 +0300

    Add project files.

---
+++ ProjectionOperatorsLinq1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ProjectionOperatorsLinq1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = Employee.GetAllEmployees().Where(x => x.AnnualSalary > 50000)
+                .Select(emp => new { Name = emp.FirstName, AnnualSalary = emp.AnnualSalary , Bonus = emp.AnnualSalary * .1 });
+
+            foreach (var v in result)
+            {
+                Console.WriteLine(v.Name+" :"  + v.AnnualSalary+ " - " + v.Bonus);
+            }
+        }
+    }
+}
